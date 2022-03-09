46e71fd075938660c9d60e24f65485a020692006
AKORA <dragoncatm@gmail.com>
2022-03-09 19:13:28 +0100

    Check compatability

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 import { config as loadConfig } from 'dotenv';
+import md5 from 'md5';
+import { writeFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 //Load enviroment variables
 loadConfig();
@@ -17,16 +20,24 @@ if (!config.USERNAME) throw new Error('Username missing');
 if (!config.PASSWORD) throw new Error('Username missing');
 if (!config.WEBDAV_URL) throw new Error('WebDav URL missing');
 
+const hash = md5(config.WEBDAV_URL + config.USERNAME + config.PASSWORD);
+
 //persistent Data
 import rawData from '../data.json';
-const data = rawData as any as {
+export const data = rawData as any as {
     hash?: string;
     lastRun?: Date;
 };
 data.lastRun ??= new Date(0);
-data.hash ??= 'a';
+data.hash ??= hash;
+
+if (data.hash !== hash) throw new Error('This data file belongs to a diffrent configuration');
+
+writeFileSync(join(__dirname, '../data.json'), JSON.stringify(data, null, 4));
 
 export const scriptStart = new Date();
 export const lastRun = data.lastRun;
 
 export default config;
+
+console.log('✅ - Loaded config');
