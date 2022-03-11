4fce2dfd76579099891dafe37383205e44a7a4f6
AKORA <dragoncatm@gmail.com>
2022-03-11 23:03:00 +0100

    Add rsync

--- src/files.ts
+++ src/files.ts
@@ -1,10 +1,21 @@
+import { execFileSync } from 'node:child_process';
 import { existsSync, mkdirSync } from 'node:fs';
 import { join } from 'node:path';
+import { gitPath } from './git';
 
 export const filesPath = join(__dirname, '../files');
-/*
+
 if (!existsSync(filesPath))
     try {
         mkdirSync(filesPath);
     } catch (e) {}
-*/
+
+export function copyWebDAV() {
+    execFileSync('rsync', [
+        '-rpt',
+        '--max-size=1m',
+        '--exclude-from=' + join(__dirname, '.gitignore'),
+        filesPath,
+        gitPath,
+    ]);
+}
