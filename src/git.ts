fe0aec119ee398a2327786d6ebca50183b14adc3
AKORA <dragoncatm@gmail.com>
2022-03-11 21:55:20 +0100

    Exec git native

--- src/git.ts
+++ src/git.ts
@@ -1,6 +1,8 @@
 import simpleGit, { SimpleGit } from 'simple-git';
 import config from './config';
 import { basename, join } from 'node:path';
+import { exec, execFileSync } from 'node:child_process';
+import { access } from 'node:fs/promises';
 
 export const gitPath = join(__dirname, '../git');
 
@@ -10,11 +12,16 @@ const GIT_URL = `http://${config.GIT_USER}:${config.GIT_PASSWORD}@${config.GIT_H
 export async function initRepo() {
     try {
         //Pull if already cloned
+        await access(gitPath);
         git = simpleGit(gitPath);
-        await pull();
+        execFileSync('git', ['pull']);
+        //await pull();
     } catch (e) {
+        console.log(e);
         //Clone if not existing yet
-        await simpleGit(join(gitPath, '..')).clone(GIT_URL, gitPath);
+        //await simpleGit(join(gitPath, '..')).clone(GIT_URL, gitPath);
+        execFileSync('git', ['clone', GIT_URL, gitPath]);
+
         git = simpleGit(gitPath);
     }
 }
