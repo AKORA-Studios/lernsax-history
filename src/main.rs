1a1faf975248035e330d728e2825058edd0fe6b4
Maf <65976562+A-K-O-R-A@users.noreply.github.com>
2022-03-13 02:27:02 +0100

    Rename files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+import './config.ts'; //Load env variables
+import './files.ts';
+
+import { commitFiles, initRepo, push } from './git.ts';
+import { copyWebDAV } from './files.ts';
+import { config } from './config.ts';
+
+let done = (_value: unknown) => {};
+const finished = new Promise((res) => (done = res));
+
+async function main() {
+    if (config.PROD) console.log('Started', new Date().toLocaleString());
+    await initRepo();
+
+    if (!config.PROD) console.log('✅ - Repo up to date');
+
+    await copyWebDAV();
+    if (!config.PROD) console.log('✅ - Synced files');
+
+    await commitFiles();
+    await push();
+    if (!config.PROD) console.log('✅ - Pushed to git');
+
+    if (config.PROD) console.log('Finished', new Date().toLocaleString(), '\n\n');
+
+    done('F');
+    //await syncWebDAV();
 }
+
+export function stop(err?: Error) {
+    console.log('Stopping');
+    //push();
+    finished.then(() => {
+        if (err) throw Error;
+        Deno.exit();
+    });
+}
+
+Deno.addSignalListener('SIGQUIT', () => {
+    console.log('SIGQUIT');
+    stop();
+});
+
+Deno.addSignalListener('SIGTERM', () => {
+    console.log('SIGTERM');
+    stop();
+});
