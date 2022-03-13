5c2e0c906414a40e217d72d9d0688a454105475b
Maf <65976562+A-K-O-R-A@users.noreply.github.com>
2022-03-13 03:11:56 +0100

    Config done

--- src/config.rs
+++ src/config.rs
@@ -10,10 +10,26 @@ pub struct Config {
     pub PROD: bool,
 }
 
-let envs = Config {
-    NODE_ENV: env::var("NODE_ENV").unwrap()
-}
+static envs: Config = Config {
+    NODE_ENV: env::var("NODE_ENV").unwrap(),
+    GIT_USER: env::var("GIT_USER").unwrap(),
+    GIT_PASSWORD: env::var("GIT_PASSWORD").unwrap(),
+    GIT_REPO: env::var("GIT_REPO").unwrap(),
+    GIT_HOST: env::var("GIT_HOST").unwrap(),
+    DEV: if env::var("NODE_ENV").unwrap() == "development" {
+        true
+    } else {
+        false
+    },
+    PROD: if env::var("NODE_ENV").unwrap() == "production" {
+        true
+    } else {
+        false
+    },
+};
 
+/*
 if (!config.GIT_USER) {throw new Error("Username missing");}
 if (!config.GIT_PASSWORD) throw new Error("Password missing");
 if (!config.GIT_HOST) throw new Error("GitHost missing");
+*/
